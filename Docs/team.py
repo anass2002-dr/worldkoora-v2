2b31d07e5bc89dcdb0b4bb2a6273f7929ad8d66f
anass2002-dr <anassdermaj163@gmail.com>
2023-09-20 13:45:40 +0100

    20-09-2023-13:45

--- Docs/team.py
+++ Docs/team.py
@@ -7,21 +7,30 @@ import csv
 # path="C:\\Program Files (x86)\\chromedriver.exe"
 # driver=webdriver.Chrome()
 
+        
 with open('C:\\xampp\\htdocs\\worldkoora\\Docs\\csv_test.csv',mode='r') as file_csv:
     csv_reader = csv.reader(file_csv, delimiter=',')
     list1=[]
     cp=0
     for ff in csv_reader:
         list1.append(ff)
-    # dictionary={}
-    # dictionary['data1']=[]
-    # for line in list1:
-    #     obj={}
-    #     obj['name']=line[0]
-    #     obj['country']=line[1]
-    #     dictionary['data1']+=obj
-    # print(dictionary)
+    dictionary={}
+    list2=[]
+   
+ 
+    
     for line in list1:
         if cp!=0:
             print(f"insert into league({cp},'{line[0]}','{line[1]}')")
+            dict1={}
+            dict1['LeagId']=cp
+            dict1[f'{list1[0][0]}']=line[0]
+            dict1[f'{list1[0][1]}']=line[1]
+            list2.append(dict1)
+            # print(dict1)
+            
         cp+=1
+    dictionary['data']=list2
+    
+    # for key,value in dictionary.items:
+    #     print(f'{key} , {value}')

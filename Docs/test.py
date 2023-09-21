8593b5af471ec9a2c76887de07021bd21714dd8a
anass2002-dr <anassdermaj163@gmail.com>
2023-09-21 14:19:00 +0100

    21-09-2023-14:18

--- Docs/test.py
+++ Docs/test.py
@@ -1,16 +1,56 @@
 import schedule
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import json
+import time
+import schedule
+# path="C:\\Program Files (x86)\\chromedriver.exe"
+# driver=webdriver.Chrome()
 
+ #hello    
+def schedule_fun():
+
+    with open('C:\\xampp\\htdocs\\worldkoora\\Docs\\csv_test.csv',mode='r') as file_csv:
+        csv_reader = csv.reader(file_csv, delimiter=',')
+        list1=[]
+        cp=0
+        for ff in csv_reader:
+            list1.append(ff)
+        dictionary={}
+        list2=[]
+        
+    
+    
+        for line in list1:
+            if cp!=0:
+                print(f"insert into league({cp},'{line[0]}','{line[1]}')")
+                dict1={}
+                dict1['LeagId']=cp
+                dict1[f'{list1[0][0]}']=line[0]
+                dict1[f'{list1[0][1]}']=line[1]
+                list2.append(dict1)
+                # print(dict1)
+                
+            cp+=1
+        dictionary['data']=list2
+        print(dictionary)
+        # with open('Docs/jsonFile.json','w') as jsonfile:
+        #     json.dump(dictionary,jsonfile)
+    # for key,value in dictionary.items:
+    #     print(f'{key} , {value}')
+schedule_fun()
+# schedule.every(3).seconds.do(schedule_fun())    
+#pip instal schedule
 def job():
     print("I'm working...")
 
-schedule.every(10).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-schedule.every().monday.do(job)
-schedule.every().wednesday.at("13:15").do(job)
-schedule.every().day.at("12:42", "Europe/Amsterdam").do(job)
-schedule.every().minute.at(":17").do(job)
+schedule.every(4).seconds.do(job)
+
 
 while True:
     schedule.run_pending()

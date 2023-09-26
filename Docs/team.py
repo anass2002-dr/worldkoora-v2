0e558356470baea35748dfaa4ce87d9268994ec3
anass dermaj <anassdermaj163@gmail.com>
2023-09-26 12:55:46 +0200

    26-09-2023:12:55

--- Docs/team.py
+++ Docs/team.py
@@ -48,129 +48,250 @@ with open('Docs\\league.csv',mode='r') as csv_league:
     for ff in csv_league:
         leagues.append(ff)
     list_leagues=[]
-    for league in leagues:
-        driver.get(league.strip())
-        dict_leagues={}
-        list_teams_infos=[]
-        title=''
-        while(title==''):
-            try:
-                title=driver.find_element(By.CLASS_NAME,'ROA474A54iOw7VMIYeSY')
-            except Exception as e:
-                print('check your connection internet brooo.....')
-                print('i''m sleeping now untile title are finding.....')
-                time.sleep(5)
-                # driver.refresh()
-                
-                title=''
-        print(title.text)   
-
-        table=object
-        table_length=''
-        while(table_length==''):
-            try:
-                table=driver.find_element(By.CLASS_NAME,'ag-center-cols-container')
-                table_length=table
-            except Exception as e:
-                print('check your connection internet brooo.....')
-                print('i''m sleeping now untile table are finding.....')
-                time.sleep(5)
-                # driver.refresh()
-                
-                table_length=''
-        print('table finding')
-
-        table_items=object
-        table_items_length=0
-        while(table_items_length==0):
-            try:
-                table_items=table.find_elements(By.CLASS_NAME,'ag-row-level-0')
-                table_items_length=len(table_items)
-            except Exception as e:
-                print('check your connection internet brooo.....')
-                print('i''m sleeping now untile table_items_length are finding.....')
-                time.sleep(5)
-                # driver.refresh()
-                
-                table_items_length=0
-        print(f'finally we find the table items of {title.text} ')  
-                      
-        cp=1
-        for item in table_items:
-            dict_teams_infos={}
+    driver.get(leagues[10].strip())
+    dict_leagues={}
+    list_teams_infos=[]
+    title=''
+    while(title==''):
+        try:
+            title=driver.find_element(By.CLASS_NAME,'ROA474A54iOw7VMIYeSY')
+        except Exception as e:
+            print('check your connection internet brooo.....')
+            print('i''m sleeping now untile title are finding.....')
+            time.sleep(5)
+            # driver.refresh()
             
-            team=object
-            team_length=''
-            while(team_length==''):
-                try:
-                    team=item.find_element(By.CLASS_NAME,'XiH7Q7qqQpsHxb_Z3I6N')
-                    team_length=team
-                except Exception as e:
-                    print('check your connection internet brooo.....')
-                    print('i''m sleeping now untile team name are finding.....')
-                    time.sleep(5)
-                    # driver.refresh()
+            title=''
+    print(title.text)   
+
+    table=object
+    table_length=''
+    while(table_length==''):
+        try:
+            table=driver.find_element(By.CLASS_NAME,'ag-center-cols-container')
+            table_length=table
+        except Exception as e:
+            print('check your connection internet brooo.....')
+            print('i''m sleeping now untile table are finding.....')
+            time.sleep(5)
+            # driver.refresh()
+            
+            table_length=''
+    print('table finding')
+
+    table_items=object
+    table_items_length=0
+    while(table_items_length==0):
+        try:
+            table_items=table.find_elements(By.CLASS_NAME,'ag-row-level-0')
+            table_items_length=len(table_items)
+        except Exception as e:
+            print('check your connection internet brooo.....')
+            print('i''m sleeping now untile table_items_length are finding.....')
+            time.sleep(5)
+            # driver.refresh()
+            
+            table_items_length=0
+    print(f'finally we find the table items of {title.text} ')  
                     
-                    team_length=''
-            team_name=team.get_attribute('title')
-            print(team_name)
-          
-            logo=object
-            logo_length=''
-            while(logo_length==''):
-                try:
-                    logo=team.find_element(By.CLASS_NAME,'pk-badge').get_attribute('src')
-                    logo_length=logo
-                except Exception as e:
-                    print('check your connection internet brooo.....')
-                    print('i''m sleeping now untile logo path are finding.....')
-                    time.sleep(5)
-                    # driver.refresh()
-                    logo_length=''
-            img_data = requests.get(logo).content
-            length_img=len(img_data)
-            path_type=img_data[-3:length_img].lower
-            path=f'images\\table_teames_logo\\{team_name}.{path_type}'
-            isExisting=os.path.exists(path)
-            if isExisting==False:
-                with open(f'images\\table_teames_logo\\{team_name}.png', 'wb') as handler:
-                    handler.write(img_data)
-    
-            infos=object
-            infos_leng=0
-            while(infos_leng<10):
-                try:
-                    infos=item.find_elements(By.CLASS_NAME,'ag-cell')
-                    infos_leng=len(infos)
-                except Exception as e:
-                    print('check your connection internet brooo.....')
-                    print('i''m sleeping now untile team infos are finding.....')
-                    time.sleep(5)
-                    # driver.refresh()
-                    infos_leng=0
-            # time.sleep(20)
-            # print(team_name)
-            dict_teams_infos['id_team']=cp
-            dict_teams_infos['team Name']=team_name
-            dict_teams_infos['logo path']=f'images\\table_teames_logo\\{team_name}.png'
-            dict_teams_infos['played']=infos[1].text
-            dict_teams_infos['won']=infos[2].text
-            dict_teams_infos['drawn']=infos[3].text
-            dict_teams_infos['lost']=infos[4].text
-            dict_teams_infos['goals']=infos[5].text
-            dict_teams_infos['against']=infos[6].text
-            dict_teams_infos['goal_differences']=infos[7].text
-            dict_teams_infos['points']=infos[8].text
-            list_teams_infos.append(dict_teams_infos)
-            cp+=1
-        dict_leagues[title]=list_teams_infos
-        with open(f'Docs/{title}.json','w') as table_1:
-            json.dump(dict_leagues,table_1)
-        list_leagues.append(dict_leagues)  
+    cp=1
+    for item in table_items:
+        dict_teams_infos={}
+        team=object
+        team_length=''
+        while(team_length==''):
+            try:
+                team=item.find_element(By.CLASS_NAME,'XiH7Q7qqQpsHxb_Z3I6N')
+                team_length=team
+            except Exception as e:
+                print('check your connection internet brooo.....')
+                print('i''m sleeping now untile team name are finding.....')
+                time.sleep(5)
+                # driver.refresh()
+                
+                team_length=''
+        team_name=team.get_attribute('title')
+        print(team_name)
         
-    table_teams_dict['table leagues']=list_leagues
-    with open('Docs/table_teams.json','w') as table_teams:
-        json.dump(table_teams_dict,table_teams)
+        logo=object
+        logo_length=''
+        while(logo_length==''):
+            try:
+                logo=team.find_element(By.CLASS_NAME,'pk-badge').get_attribute('src')
+                logo_length=logo
+            except Exception as e:
+                print('check your connection internet brooo.....')
+                print('i''m sleeping now untile logo path are finding.....')
+                time.sleep(5)
+                # driver.refresh()
+                logo_length=''
+        img_data=''
+        if logo is None:
+            img_data=b'images\\table_teames_logo\\football.png'
+        else:
+            img_data = requests.get(logo).content
+        
+        path=f'images\\table_teames_logo\\{team_name}.png '
+        isExisting=os.path.exists(path)
+        if isExisting==False:
+            with open(f'images\\table_teames_logo\\{team_name}.png', 'wb') as handler:
+                handler.write(img_data)
+
+        infos=object
+        infos_leng=0
+        while(infos_leng<10):
+            try:
+                infos=item.find_elements(By.CLASS_NAME,'ag-cell')
+                infos_leng=len(infos)
+            except Exception as e:
+                print('check your connection internet brooo.....')
+                print('i''m sleeping now untile team infos are finding.....')
+                time.sleep(5)
+                # driver.refresh()
+                infos_leng=0
+        # time.sleep(20)
+        # print(team_name)
+        dict_teams_infos['id_team']=cp
+        dict_teams_infos['team Name']=team_name
+        dict_teams_infos['logo path']=f'images\\table_teames_logo\\{team_name}.png'
+        dict_teams_infos['played']=infos[1].text
+        dict_teams_infos['won']=infos[2].text
+        dict_teams_infos['drawn']=infos[3].text
+        dict_teams_infos['lost']=infos[4].text
+        dict_teams_infos['goals']=infos[5].text
+        dict_teams_infos['against']=infos[6].text
+        dict_teams_infos['goal_differences']=infos[7].text
+        dict_teams_infos['points']=infos[8].text
+        list_teams_infos.append(dict_teams_infos)
+        cp+=1
+    dict_leagues[title.text]=list_teams_infos
+    # print(dict_leagues)
+    with open(f'Docs/teams_table/{title.text}.json','w') as table_1:
+        json.dump(dict_leagues,table_1)
+    # for league in leagues:
+    #     driver.get(league.strip())
+    #     dict_leagues={}
+    #     list_teams_infos=[]
+    #     title=''
+    #     while(title==''):
+    #         try:
+    #             title=driver.find_element(By.CLASS_NAME,'ROA474A54iOw7VMIYeSY')
+    #         except Exception as e:
+    #             print('check your connection internet brooo.....')
+    #             print('i''m sleeping now untile title are finding.....')
+    #             time.sleep(5)
+    #             # driver.refresh()
+                
+    #             title=''
+    #     print(title.text)   
+
+    #     table=object
+    #     table_length=''
+    #     while(table_length==''):
+    #         try:
+    #             table=driver.find_element(By.CLASS_NAME,'ag-center-cols-container')
+    #             table_length=table
+    #         except Exception as e:
+    #             print('check your connection internet brooo.....')
+    #             print('i''m sleeping now untile table are finding.....')
+    #             time.sleep(5)
+    #             # driver.refresh()
+                
+    #             table_length=''
+    #     print('table finding')
+
+    #     table_items=object
+    #     table_items_length=0
+    #     while(table_items_length==0):
+    #         try:
+    #             table_items=table.find_elements(By.CLASS_NAME,'ag-row-level-0')
+    #             table_items_length=len(table_items)
+    #         except Exception as e:
+    #             print('check your connection internet brooo.....')
+    #             print('i''m sleeping now untile table_items_length are finding.....')
+    #             time.sleep(5)
+    #             # driver.refresh()
+                
+    #             table_items_length=0
+    #     print(f'finally we find the table items of {title.text} ')  
+                      
+    #     cp=1
+    #     for item in table_items:
+    #         dict_teams_infos={}
+            
+    #         team=object
+    #         team_length=''
+    #         while(team_length==''):
+    #             try:
+    #                 team=item.find_element(By.CLASS_NAME,'XiH7Q7qqQpsHxb_Z3I6N')
+    #                 team_length=team
+    #             except Exception as e:
+    #                 print('check your connection internet brooo.....')
+    #                 print('i''m sleeping now untile team name are finding.....')
+    #                 time.sleep(5)
+    #                 # driver.refresh()
+                    
+    #                 team_length=''
+    #         team_name=team.get_attribute('title')
+    #         print(team_name)
+          
+    #         logo=object
+    #         logo_length=''
+    #         while(logo_length==''):
+    #             try:
+    #                 logo=team.find_element(By.CLASS_NAME,'pk-badge').get_attribute('src')
+    #                 logo_length=logo
+    #             except Exception as e:
+    #                 print('check your connection internet brooo.....')
+    #                 print('i''m sleeping now untile logo path are finding.....')
+    #                 time.sleep(5)
+    #                 # driver.refresh()
+    #                 logo_length=''
+    #         img_data = requests.get(logo).content
+    #         length_img=len(img_data)
+    #         path_type=img_data[-3:length_img].lower
+    #         path=f'images\\table_teames_logo\\{team_name}.{path_type}'
+    #         isExisting=os.path.exists(path)
+    #         if isExisting==False:
+    #             with open(f'images\\table_teames_logo\\{team_name}.png', 'wb') as handler:
+    #                 handler.write(img_data)
+    
+    #         infos=object
+    #         infos_leng=0
+    #         while(infos_leng<10):
+    #             try:
+    #                 infos=item.find_elements(By.CLASS_NAME,'ag-cell')
+    #                 infos_leng=len(infos)
+    #             except Exception as e:
+    #                 print('check your connection internet brooo.....')
+    #                 print('i''m sleeping now untile team infos are finding.....')
+    #                 time.sleep(5)
+    #                 # driver.refresh()
+    #                 infos_leng=0
+    #         # time.sleep(20)
+    #         # print(team_name)
+    #         dict_teams_infos['id_team']=cp
+    #         dict_teams_infos['team Name']=team_name
+    #         dict_teams_infos['logo path']=f'images\\table_teames_logo\\{team_name}.png'
+    #         dict_teams_infos['played']=infos[1].text
+    #         dict_teams_infos['won']=infos[2].text
+    #         dict_teams_infos['drawn']=infos[3].text
+    #         dict_teams_infos['lost']=infos[4].text
+    #         dict_teams_infos['goals']=infos[5].text
+    #         dict_teams_infos['against']=infos[6].text
+    #         dict_teams_infos['goal_differences']=infos[7].text
+    #         dict_teams_infos['points']=infos[8].text
+    #         list_teams_infos.append(dict_teams_infos)
+    #         cp+=1
+    #     dict_leagues[title.text]=list_teams_infos
+    #     # print(dict_leagues)
+    #     with open(f'Docs/teams_table/{title.text}.json','w') as table_1:
+    #         json.dump(dict_leagues,table_1)
+    #     list_leagues.append(dict_leagues)  
+        
+    # table_teams_dict['table leagues']=list_leagues
+    # with open('Docs/teams_table/table_teams.json','w') as table_teams:
+    #     json.dump(table_teams_dict,table_teams)
     
     
     
